346f324e48a84476288ff2127935092d3c81d36a
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-01 12:07:40 +0100

    Solution to 3sum problem: trial 1

--- TwoPointers/3Sum/3Sum.js
+++ TwoPointers/3Sum/3Sum.js
@@ -3,46 +3,31 @@
  * @return {number[][]}
  */
 var threeSum = function (nums = [-1, 0, 1, 2, -1, -4]) {
-  nums.sort((a, b) => a - b);
+  nums.sort();
   let i = 0,
-    j = 1,
-    k = 2,
-    answers = [];
+    j,
+    k,
+    mapper = new Map(),
+    answer = [];
 
-  while (
-    i < nums.length - 1 &&
-    nums[i] !== nums[j] &&
-    nums[j] !== nums[k] &&
-    nums[i] !== nums[k]
-  ) {
-    if (nums[i] + nums[j] + nums[k] < 0) {
-      console.log("plus", nums, nums[k], k);
-      k++;
-    }
+  for (let i = 0; i < nums.length / 2; i++) {
+    for (let j = nums.length - 1; j >= 0; j--) {
+      for (let k = 0; k < nums.length; k++) {
+        if (i !== j && i !== k && j !== k) {
+          let complement = 0 - nums[k];
+          mapper[k] = complement;
 
-    if (nums[i] + nums[j] + nums[k] > 0) {
-      console.log("minus");
-      k--;
-    }
+          if (nums[i] + nums[j] === complement) {
+            let ans = [nums[i], nums[j], nums[k]].toSorted();
 
-    if (nums[i] + nums[j] + nums[k] === 0) {
-      console.log("good");
-      answers.push([nums[i], nums[j], nums[k]]);
-    }
-
-    if (j === nums.length - 1) {
-      i++;
-      j = nums.length - i;
-      k = nums.length - j;
-      console.log(i, j, k);
-    } else if (k === nums.length - 1) {
-      j++;
-      k = nums.length - j;
-      console.log("k is above", k, j);
+            answer.push(ans);
+          }
+        }
+      }
     }
   }
 
-  return answers;
+  return answer;
 };
 
 console.log(threeSum());
