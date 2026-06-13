c7ace99da3db4d69d834f010d695ca59f2f63814
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-13 15:36:56 +0100

    wip: nitty gritty

--- TwoPointers/TrappingRainWater/TrappingRainWater.js
+++ TwoPointers/TrappingRainWater/TrappingRainWater.js
@@ -4,24 +4,48 @@
  */
 
 var trap = function (height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) {
-  let left = 0,
-    right = height.length - 1,
-    maxLeft = 0,
-    maxRight = 0,
-    total = 0;
+  let total = 0,
+    left = 0,
+    right = 1;
 
   while (left < right) {
-    if (height[left] <= height[right]) {
-      // Left side is the bottleneck — water level determined by maxLeft
-      if (height[left] >= maxLeft) maxLeft = height[left];
-      else total += maxLeft - height[left];
-      left++;
-    } else {
-      // Right side is the bottleneck — water level determined by maxRight
-      if (height[right] >= maxRight) maxRight = height[right];
-      else total += maxRight - height[right];
-      right--;
+    // console.log(left, right, height[left], height[right]);
+    for (let i = left + 1; i < height.length; i++) {
+      let max = height[left];
+
+      if (max < height[i]) {
+        max = height[i];
+        // console.log("less");
+        right = i;
+        break;
+      } else if (max === height[i]) {
+        right = i;
+        break;
+      }
+
+      // console.log("maxing");
     }
+
+    console.log(left, right);
+
+    if (right - left > 1) {
+      // console.log("hello world", right - left);
+      let diff = right - left,
+        h = left > right ? height[right] : height[left],
+        tot = (diff - 1) * h;
+      // console.log("tot:", tot, "h:", h);
+
+      for (i = 1; i < diff; i++) {
+        // console.log("tots: ", tot, "curr: ", height[left + i], "i:", i);
+        tot -= height[left + i];
+      }
+
+      total += tot;
+    }
+
+    left = right;
+    height.length - 1 > right ? right++ : right;
+    console.log("total:", total);
   }
 
   return total;
@@ -29,3 +53,27 @@ var trap = function (height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) {
 
 console.log(trap()); // 6
 console.log(trap([4, 2, 0, 3, 2, 5])); // 9
+
+/*
+
+# 
+
+##
+# 
+
+# 
+###
+##
+# 
+##
+#
+
+
+// //////////////////////////////
+####
+##
+
+###
+##
+#####
+*/
