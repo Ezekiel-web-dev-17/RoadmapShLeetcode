1a2739cf174028d378c6c8d3ec00455d7391c23e
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-14 02:01:44 +0100

    wip: failed 147th test

--- TwoPointers/TrappingRainWater/TrappingRainWater.js
+++ TwoPointers/TrappingRainWater/TrappingRainWater.js
@@ -9,34 +9,25 @@ var trap = function (height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) {
     right = 1;
 
   while (left < right) {
-    // console.log(left, right, height[left], height[right]);
     for (let i = left + 1; i < height.length; i++) {
       let max = height[left];
 
       if (max < height[i]) {
         max = height[i];
-        // console.log("less");
         right = i;
         break;
       } else if (max === height[i]) {
         right = i;
         break;
       }
-
-      // console.log("maxing");
     }
 
-    console.log(left, right);
-
     if (right - left > 1) {
-      // console.log("hello world", right - left);
       let diff = right - left,
         h = left > right ? height[right] : height[left],
         tot = (diff - 1) * h;
-      // console.log("tot:", tot, "h:", h);
 
       for (i = 1; i < diff; i++) {
-        // console.log("tots: ", tot, "curr: ", height[left + i], "i:", i);
         tot -= height[left + i];
       }
 
@@ -45,13 +36,13 @@ var trap = function (height = [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]) {
 
     left = right;
     height.length - 1 > right ? right++ : right;
-    console.log("total:", total);
   }
 
   return total;
 };
 
 console.log(trap()); // 6
+console.log(trap([4, 2, 3])); // 1
 console.log(trap([4, 2, 0, 3, 2, 5])); // 9
 
 /*
