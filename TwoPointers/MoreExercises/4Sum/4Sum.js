f97b5524a12c8afb622700112aa51cd7136cf5fd
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-16 08:51:43 +0100

    docs: setup for more Exercises - 4Sum

---
+++ TwoPointers/MoreExercises/4Sum/4Sum.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[][]}
+ */
+var fourSum = function (nums = [1, 0, -1, 0, -2, 2], target = 0) {
+    console.log(nums)
+
+    return;
+};
+
+console.log(fourSum())
