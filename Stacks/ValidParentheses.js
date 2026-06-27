3f9a3312e52d33fd2fccaf504504f8376c95c52e
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-27 12:48:23 +0100

    feat: gregg method

--- Stacks/ValidParentheses.js
+++ Stacks/ValidParentheses.js
@@ -14,18 +14,12 @@ var isValid = function (s = "()[]{}") {
     tmpStack = [];
 
   for (let i = 0; i < s.length; i++) {
-    const char = s.charAt(i);
-
-    if (opens.includes(char)) {
-      // When you encounter an opening bracket, push it to the top of the stack.
-      stack = [...stack.filter((stc) => stc !== char)];
-      tmpStack.push(char);
-    } else if (tmpStack.length && map[char] === tmpStack[tmpStack.length - 1]) {
-      //   When you encounter a closing bracket, check if the top of the stack was the opening for it. If yes, pop it from the stack. Otherwise, return false.
-      stack = [...stack.filter((stc) => stc)];
+    tmpStack.push(stack.pop());
+    if (
+      map[tmpStack[tmpStack.length - 1]] === stack[stack.length - 1] ||
+      opens.includes(stack[stack.length - 1])
+    )
       tmpStack.pop();
-      console.log("tmpStack", tmpStack);
-    }
   }
 
   return tmpStack.length ? false : true;
