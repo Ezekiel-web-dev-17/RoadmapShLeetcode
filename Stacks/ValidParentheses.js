7f3b7ca9b2358b327ff06efc27f51c603f578d5a
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-06-28 17:41:59 +0100

    feat: implementation 5

--- Stacks/ValidParentheses.js
+++ Stacks/ValidParentheses.js
@@ -11,43 +11,48 @@ var isValid = function (s = "()[]{}") {
     };
 
   let stack = s.split(""), // stack of characters.
-    tmpStack = [];
+    tmpStack = [],
+    remnant = [];
 
   stack = stack.map((stk) => {
     if (open.includes(stk)) {
       tmpStack.push(stk);
-    } else return stk;
+    }
+
+    return stk;
   });
 
-  // console.log("stack: ", stack, "tmpStack: ", tmpStack);
+  const a = stack.length - 1;
 
-  for (let i = stack.length - 1; i >= 0; i--) {
-    // console.log(
-    //   stack[i],
-    //   tmpStack[tmpStack.length - 1],
-    //   map[tmpStack[tmpStack.length - 1]],
-    //   map[tmpStack[tmpStack.length - 1]] === stack[i],
-    // );
-    if (
-      stack[i] !== undefined &&
-      map[tmpStack[tmpStack.length - 1]] === stack[i]
-    ) {
-      // console.log("first", stack);
-      stack[i] = undefined;
-      i = s.length;
-      // console.log("2nd", stack);
+  for (let i = a; i >= 0; i--) {
+    if (map[tmpStack[tmpStack.length - 1]] === stack[stack.length - 1]) {
+      stack.pop();
       tmpStack.pop();
+    } else if (open.includes(stack[i])) {
+      stack.pop();
+    } else if (tmpStack[tmpStack.length - 1] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      remnant.push(stack.pop());
     }
   }
 
-  // console.log("stack: ", stack, "tmpStack: ", tmpStack);
+  for (let b = 0; b < tmpStack.length; b++) {
+    if (map[tmpStack[tmpStack.length - 1]] === remnant[remnant.length - 1]) {
+      tmpStack.pop();
+      remnant.pop();
+    }
+  }
 
-  return tmpStack.length ? false : true;
+  return tmpStack.length === 0 && remnant.length === 0 ? true : false;
 };
 
 console.log(isValid());
 console.log(isValid("()"));
 console.log(isValid("(]"));
+console.log("#######################");
+console.log(isValid("([)]"));
+console.log("!!!!!!!!!!!!!!!!!!!!!!!");
 console.log(isValid("([])"));
 console.log(isValid("({{{{}}}))")); // false
 console.log(isValid("()))")); // false
