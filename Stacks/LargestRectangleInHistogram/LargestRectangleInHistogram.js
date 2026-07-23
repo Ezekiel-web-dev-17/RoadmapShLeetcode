fa27c5c4523f9c84183a4e540c24c2e299efcad1
Ezekiel-web-dev-17 <tabukeezekiel9@gmail.com>
2026-07-23 19:42:19 +0100

    Attempt 2

--- Stacks/LargestRectangleInHistogram/LargestRectangleInHistogram.js
+++ Stacks/LargestRectangleInHistogram/LargestRectangleInHistogram.js
@@ -3,32 +3,24 @@
  * @return {number}
  */
 var largestRectangleArea = function (heights = [2, 1, 5, 6, 2, 3]) {
-  let stack = [],
-    max = 0,
+  let stack = [heights[0]],
+    max = heights[0],
     start = 0;
 
-  for (let i = 0; i < heights.length; i++) {
-    stack.push(heights[i]);
-    lastInStack = stack.slice(-2, -1).pop() ?? 0;
-    h = Math.min(heights[i], lastInStack);
-    s = start ? i - start : i;
-    max = Math.max(max, h * (s + 1));
-    if (max <= heights[i]) {
+  for (let i = 1; i < heights.length; i++) {
+    if (heights[i] >= stack[stack.length - 1]) {
+      stack.push(heights[i]);
+    } else {
       stack = [heights[i]];
-      start = i;
-      max = heights[i];
-    }
-    if (lastInStack < heights[i]) {
-      stack = [lastInStack, heights[i]];
-      start = i;
     }
 
-    console.log(stack, max, start, i, heights[i]);
+    for (let j = 0; j < stack.length; j++) {
+      max = Math.max(max, stack[j] * (stack.length - j), stack[j]);
+    }
   }
 
   return max;
 };
-
 console.log(largestRectangleArea());
 console.log(largestRectangleArea([2, 1, 2]));
 console.log(largestRectangleArea([0, 9]));
